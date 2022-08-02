7c241e1594e23104aa868438cb2e8945e5125494
m1504 <m15043340061@163.com>
2022-08-02 20:52:47 +0800

    Update Fabric to 1.19.1. Support more minecraft version.

--- src/main/java/com/laosun/stackone/StackOneMod.java
+++ src/main/java/com/laosun/stackone/StackOneMod.java
@@ -3,22 +3,17 @@ package com.laosun.stackone;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.util.registry.Registry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 
 public class StackOneMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("stackone");
 
 	@Override
 	public void onInitialize() {
 		for (Item i : Registry.ITEM) {
 			try {
-				LOGGER.debug(i.getTranslationKey());
 				Class<? extends Item> a = Item.class;
 				Field f2;
 				try {
