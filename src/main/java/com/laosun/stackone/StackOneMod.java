859713703336e5c657216a4c9d5786b27d6a1fd6
m1504 <m15043340061@163.com>
2024-09-02 12:44:13 +0800

    fix #19

--- src/main/java/com/laosun/stackone/StackOneMod.java
+++ src/main/java/com/laosun/stackone/StackOneMod.java
@@ -1,12 +1,19 @@
 package com.laosun.stackone;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.player.PlayerContainerEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.item.Item;
 import org.slf4j.Logger;
 
 import java.lang.reflect.Field;
@@ -19,8 +26,8 @@ public class StackOneMod {
 
     public StackOneMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
         modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SuppressWarnings("deprecation")
@@ -47,6 +54,55 @@ public class StackOneMod {
                 a.set(i, 1);
             } catch (IllegalAccessException e) {
                 LOGGER.error("Failed to set field!!!");
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onItemPickup(PlayerEvent.ItemPickupEvent event) {
+        Player player = event.getEntity();
+        ItemStack stack = event.getStack();
+
+        if (stack.getCount() > 1) {
+            splitStacksInInventory(player);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onItemCrafted(PlayerEvent.ItemCraftedEvent event) {
+        Player player = event.getEntity();
+        ItemStack stack = event.getCrafting();
+
+        if (stack.getCount() > 1) {
+            splitStacksInInventory(player);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onItemSmelted(PlayerEvent.ItemSmeltedEvent event) {
+        Player player = event.getEntity();
+        ItemStack stack = event.getSmelting();
+
+        if (stack.getCount() > 1) {
+            splitStacksInInventory(player);
+        }
+    }
+
+    private void splitStacksInInventory(Player player) {
+        for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
+            ItemStack stack = player.getInventory().getItem(i);
+
+            if (stack.getCount() > 1) {
+                int stackSize = stack.getCount();
+                stack.setCount(1);
+
+                for (int j = 1; j < stackSize; j++) {
+                    ItemStack singleItemStack = stack.copy();
+
+                    if (!player.getInventory().add(singleItemStack)) {
+                        player.drop(singleItemStack, false);
+                    }
+                }
             }
         }
     }
